479d4020643f00874b18d53e4e1a20c831137e1c
JahanzebKhan98 <80027665+JahanzebKhan98@users.noreply.github.com>
2022-04-26 00:27:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
+import mysql.connector
+ 
+# Creating connection object
+mydb = mysql.connector.connect(
+	
+    host = "localhost",
+    user = "jk",
+    password = "getmein123",
+    database = "testDB"
+)
+ 
+
+
+
+
+
+
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Jahanzeb Khan!"
+
+@app.route("/") 
+def Hello():
+    return "Hello from jahanazeb khan"
+
+
+
+
+@app.route("/get_result") 
+def get_results():
+    lis = []
+    cursor = mydb.cursor()
+    cursor.execute('SELECT * FROM Persons')
+    # cursor.execute("SHOW DATABASE")
+    # Printing the connection object
+
+    myresult = cursor.fetchall()
+    for i in myresult:
+        lis.append(i)
+        
+    return {"result":lis}
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
